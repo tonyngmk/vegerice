a289e122c7d3792519e1c8e83c1ac21810e103e4
Tony <tonyngmengkiat@gmail.com>
2023-09-17 14:56:30 +0800

    implemented github sign in with next-auth

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,21 @@
 import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github"
 
-import { authOptions } from "~/server/auth";
+// import { authOptions } from "~/server/auth";
+
+const githubClientId: string = process.env.GITHUB_ID !== undefined ? process.env.GITHUB_ID : "";
+const githubClientSecret: string = process.env.GITHUB_CLIENT_SECRET !== undefined ? process.env.GITHUB_CLIENT_SECRET : "";
+
+export const authOptions = {
+    // Configure one or more authentication providers
+    providers: [
+      GithubProvider({
+        clientId: githubClientId,
+        clientSecret: githubClientSecret,
+      }),
+      // ...add more providers here
+    ],
+  }
+  
 
 export default NextAuth(authOptions);
