536e857b70333d712a24ddc7e2093907fe45dec3
Tony <tonyngmengkiat@gmail.com>
2023-09-17 15:02:15 +0800

    update to vercel nextauth url

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -3,8 +3,8 @@ import GithubProvider from "next-auth/providers/github"
 
 // import { authOptions } from "~/server/auth";
 
-const githubClientId: string = process.env.GITHUB_ID !== undefined ? process.env.GITHUB_ID : "";
-const githubClientSecret: string = process.env.GITHUB_CLIENT_SECRET !== undefined ? process.env.GITHUB_CLIENT_SECRET : "";
+const githubClientId: string = process.env.GITHUB_ID ?? "";
+const githubClientSecret: string = process.env.GITHUB_CLIENT_SECRET ?? "";
 
 export const authOptions = {
     // Configure one or more authentication providers
